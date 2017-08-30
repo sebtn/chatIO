612ee95ec2b9661bf2f56b6d82ef748a08dfc88e
sebtn <sdebedout@gmail.com>
2017-08-29 22:51:12 -0400

    Express server connected

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
-console.log(__dirname)
-console.log(__dirname  + '/../public' )
-let path = require('path')
+const path = require('path')
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public') 
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath)) // serve static HTML
+app.listen(port)
+console.log('Express is now in charge of port: ' + port)
