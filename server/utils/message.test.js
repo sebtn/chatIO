bec2ae5c410463ee7e561c48600b7c87e5df67ae
sebtn <sdebedout@gmail.com>
2017-08-30 13:54:55 -0400

    Geolocation test refactored

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,6 @@
 var expect = require('expect')
-var {generateMessage} = require('./message')
+var { generateMessage, 
+    generateLocationMessage } = require('./message')
 
 describe('generateMessage', () => {
 
@@ -11,6 +12,21 @@ describe('generateMessage', () => {
     expect(message.createdAt).toBeA('number')
     expect(message.from).toMatch('Seb')
     expect(message.text).toMatch('Some text')
+  })
+
+})
+
+describe('generateLocationMessage', () => {
+
+  it('should generate correct location object', () => {
+    const from = 'Seb'
+    const latitude = 1
+    const longitude = 1
+    const url = 'https://www.google.ca/maps/?q=1,1'
+    const message = generateLocationMessage(from, latitude, longitude)
+   
+    expect(message.createdAt).toBeA('number')
+    expect(message).toInclude({from, url})
   })
 
 })
