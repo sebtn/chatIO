798cef4ed83c58700c881191b6c3200fb9dc6d71
sebtn <sdebedout@gmail.com>
2017-08-29 23:36:24 -0400

    Connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
+const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public') 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketIO(server) // server - client
 
 app.use(express.static(publicPath)) // serve static HTML
-app.listen(port)
-console.log(`Express is now in charge of port: ${port}`)
+
+// persistent connection
+io.on('connection', (socket) => {
+  console.log('new user connected and logged from server')
+
+  socket.on('disconnect', () => {
+    console.log('disconnected and logged from server')
+  })
+
+})
+
+server.listen(port, () => {
+  console.log(`Express is now in charge of port: ${port}`)
+})
