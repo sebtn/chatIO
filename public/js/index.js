1fbfb954cdcd23e1792bd959262984e9d57e51b9
sebtn <sdebedout@gmail.com>
2017-08-30 20:05:35 -0400

    moment added and connected

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,21 @@ socket.on('disconnect', function() {
 })
 
 /*-----------------------------------------------------*/
-socket.on('newMessage', function(newMessage) {
+//value passed into moment
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a')
   var li = jQuery('<li></li>')
-  li.text(`${newMessage.from}: ${newMessage.text}`)
+  li.text(`${message.from} ${formattedTime}: ${message.text}`)
   jQuery('#messages-list').append(li)
 })
 
 /*-----------------------------------------------------*/
 // generate dom
 socket.on('newLocationMessage', function(message) { 
+  var formattedTime = moment(message.createdAt).format('h:mm a')
   var li = jQuery('<li></li>')
   var a = jQuery('<a target="_blank">Current location</a>')
-  li.text(`${message.from}: `)
+  li.text(`${message.from} ${formattedTime}: `)
   a.attr('href', message.url)
   li.append(a)
   jQuery('#messages-list').append(li)
@@ -31,7 +34,7 @@ var messageTextBox = jQuery('[name=message]')
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault()
   socket.emit('createMessage', {
-    from: 'user',
+    from: 'User',
     text: messageTextBox.val()
   }, function() { //cb is acknowledgment
     messageTextBox.val('')
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,16 +1,20 @@
+const moment = require('moment')
+
+/*---------------------------------------------------------------*/
 const generateMessage = (from, text) => {
   return {
     from,
     text,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   }
 }
-// https://www.google.ca/maps/?q=45.5016889,-73.56725599999999
+
+/*---------------------------------------------------------------*/
 const generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
     url: `https://www.google.ca/maps/?q=${latitude},${longitude}`,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   }
 }
 
